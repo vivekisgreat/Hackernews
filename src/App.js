a7f5acf0d12402cb003bd0bf57b9c1e8e42668bc
Vivek Kumar Verma <vivek.kverma.civ17@itbhu.ac.in>
2020-05-30 15:35:28 +0530

    second exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    var helloWorld = 'Welcome to the Road to learn React';
+    var user ={
+      firstName:"vivek",
+      lastName:"verma",
+      age:20
+    }
+    return (
+      <div className="App">
+        <h2> {helloWorld} </h2>
+        <p>{user.firstName} {user.lastName} is {user.age} years old</p>
+      </div>
+    );
+  }
 }
 
 export default App;
