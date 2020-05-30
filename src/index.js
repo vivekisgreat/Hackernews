54dfc56b1cc7d1aa8e67c0717cf28b8cd47a6bb4
Vivek Kumar Verma <vivek.kverma.civ17@itbhu.ac.in>
2020-05-30 17:34:02 +0530

    react dom changed

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello React World</h1>,
   document.getElementById('root')
 );
 
